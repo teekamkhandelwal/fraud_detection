9618ec6a81f2824125eecd6497c373c074a9a933
teekamkhandelwal <teekamkhandelwal@gmail.com>
2023-01-29 17:04:04 +0530

    added 3 new ml mode code

--- model.py
+++ model.py
@@ -1,2 +1,16 @@
  
 print("add all machine lerning code here")
+
+def model1():
+    print("adding code for ml model1")
+
+
+def model2():
+    print("adding code for ml model 2")
+
+
+def model3():
+    print("adding code for model 3")
+
+
+    
